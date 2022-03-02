94322fdf4b67ebff1a4495381a685bb8751c2706
jinow9595 <jinow9595@gmail.com>
2022-03-03 00:43:42 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const clickEvent = () => setCounter(test => test + 1);
   return (
     <div>
-      <h1>hello</h1>
+      <h1 className={styles.title}>hello</h1>
+      <Button text={"nothing"} />
+      <button onClick={clickEvent}>{counter}</button>
     </div>
   );
 }
