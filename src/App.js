76966fae1ad682f23ad1666665d82a0f7eca29ea
jinow9595 <jinow9595@gmail.com>
2022-03-18 23:23:06 +0900

    useEffect array value

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const clickEvent = () => setCounter(test => test + 1);
+  const changeEvent = (event) => {
+    setKeyword(event.target.value)
+  };
 
   console.log("run everytime");
 
@@ -14,8 +18,13 @@ function App() {
 
   useEffect(once, []);
 
+  useEffect(() => {
+    console.log("search for", keyword);
+  }, [keyword]); //keyword가 변경될 때마다 실행될 메서드
+
   return (
     <div>
+      <input onChange={changeEvent} value={keyword} type="text" placeholder="search..." />
       <h1 className={styles.title}>hello</h1>
       <Button text={"nothing"} />
       <button onClick={clickEvent}>{counter}</button>
