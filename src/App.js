4c94e2241173076c44f9c396833bfbb25e5b831e
jinow9595 <jinow9595@gmail.com>
2022-03-29 13:49:26 +0900

    css 추가하기

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    // gh pages 배포 시 router에 basename 추가 필요
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/movie" element={<Detail />}>
           <Route path=":id" element={<Detail />} />
